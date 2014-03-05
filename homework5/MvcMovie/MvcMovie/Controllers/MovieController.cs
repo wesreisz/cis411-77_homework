7abafc8197a2f9934c86ae9bfebf8a5d1716ca53
Zach Woodward <zachw38@gmail.com>
2014-03-05 15:34:55 -0500

    Got all the search part done. Shit's pretty slick!

--- homework5/MvcMovie/MvcMovie/Controllers/MovieController.cs
+++ homework5/MvcMovie/MvcMovie/Controllers/MovieController.cs
@@ -15,15 +15,29 @@ namespace MvcMovie.Controllers
         private MvcMovieContext db = new MvcMovieContext();
 
         // GET: /Movie/
-        public ActionResult Index(string searchString)
+        public ActionResult Index(string movieGenre, string searchString)
         {
+            var GenreLst = new List<string>();
+
+            var GenreQry = from d in db.Movies
+                           orderby d.Genre
+                           select d.Genre;
+
+            GenreLst.AddRange(GenreQry.Distinct());
+            ViewBag.movieGenre = new SelectList(GenreLst);
+
             var movies = from m in db.Movies
                          select m;
 
             if (!String.IsNullOrEmpty(searchString))
             {
                 movies = movies.Where(s => s.Title.Contains(searchString));
-            } 
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
 
             return View(movies);
         }
