c04e22fb8d39a387f76999ab4105403f69c498f7
Joseph Blackett <joseph.blackett@gmail.com>
2014-03-07 17:18:29 -0500

    added welcome view

--- homework6/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ homework6/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -24,9 +24,12 @@ namespace MvcMovie.Controllers
         //
         // Get: /HelloWorld/Welcome/
 
-        public string Welcome(string name, int ID = 1)
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
         }
 
 
