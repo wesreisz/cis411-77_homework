a4a75d2ad3f78112b7f958f3264113c111ee1c28
Zach Woodward <zachw38@gmail.com>
2014-03-19 16:53:18 -0400

    Initial commit + Controller and Model added

---
+++ homework6/PetStore/PetStore/Models/Pet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace PetStore.Models
+{
+    public class Pet
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateReceived { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class PetDBContext : DbContext
+    {
+        public DbSet<Pet> Pets { get; set; }
+    }
+}
