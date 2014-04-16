4852eaf6b445800fff5637f53ab1ad48942a1ebe
Joseph Blackett <joseph.blackett@gmail.com>
2014-04-16 16:23:40 -0400

    created classes

---
+++ homework7/ProductStore/ProductStore/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductStore.Models
+{
+    public class Order
+    {
+        public class Order
+        {
+            public int Id { get; set; }
+            [Required]
+            public string Customer { get; set; }
+
+            // Navigation property
+            public ICollection<OrderDetail> OrderDetails { get; set; }
+        }
+    }
+}
